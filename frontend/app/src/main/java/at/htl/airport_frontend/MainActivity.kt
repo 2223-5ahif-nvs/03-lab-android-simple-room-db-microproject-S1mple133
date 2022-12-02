87683eaa66759b1ccaa39d272f614e1582f9485b
S1mple133 <pavelescu_darius@yahoo.com>
2022-12-02 10:33:59 +0100

    added basic card for flight

--- frontend/app/src/main/java/at/htl/airport_frontend/MainActivity.kt
+++ frontend/app/src/main/java/at/htl/airport_frontend/MainActivity.kt
@@ -3,18 +3,23 @@ package at.htl.airport_frontend
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import at.htl.airport_frontend.entity.Flight
 import at.htl.airport_frontend.ui.theme.AirportfrontendTheme
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+const val dateTimeFormat: String = "dd.MM.yyyy hh:mm";
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,32 +47,46 @@ fun FlightList() {
 @Composable
 fun DefaultPreview() {
     AirportfrontendTheme {
-        FlightCard(Flight(airportIcao = "ICAO",
-            departure = LocalDateTime.now(),
-            arrival = LocalDateTime.now().plusDays(1),
-            flightNumber = 41,
-            flightType = "ARRIVAL"
-        ));
+        FlightCard(
+            Flight(
+                airportIcao = "ICAO",
+                departure = LocalDateTime.now(),
+                arrival = LocalDateTime.now().plusDays(1),
+                flightNumber = 41,
+                flightType = "ARRIVAL"
+            )
+        );
     }
 }
 
 @Composable
 fun FlightCard(flight: Flight) {
-    Column() {
-        Row() {
-            Text(text = "Type: " + flight.flightType)
-        }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column() {
+                Text(text = "Type")
+                Text(text = "Departure")
+                Text(text = "Arrival")
+                Text(text = "Airport")
+            }
 
-        Row() {
-            Text(text = "Departure: " + flight.departure)
-        }
-
-        Row() {
-            Text(text = "Type: " + flight.flightType)
-        }
-
-        Row() {
-            Text(text = "Type: " + flight.flightType)
+            Column() {
+                Text(text = flight.flightType)
+                Text(text = formatDateTime(flight.departure))
+                Text(text = formatDateTime(flight.arrival))
+                Text(text = flight.airportIcao)
+            }
         }
     }
+}
+
+fun formatDateTime(dateTime: LocalDateTime): String {
+    return dateTime.format(DateTimeFormatter.ofPattern(dateTimeFormat));
 }
