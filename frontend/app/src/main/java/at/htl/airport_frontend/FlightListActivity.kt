621c51a6e0a97845f05c98f9d6195ed8efdcff40
S1mple133 <pavelescu_darius@yahoo.com>
2023-03-10 15:17:34 +0100

    added alert when airport is marked as favourite

--- frontend/app/src/main/java/at/htl/airport_frontend/FlightListActivity.kt
+++ frontend/app/src/main/java/at/htl/airport_frontend/FlightListActivity.kt
@@ -1,5 +1,6 @@
 package at.htl.airport_frontend
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,13 +11,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -72,35 +73,50 @@ fun FlightCard(flight: FlightDto, openAirportActivity: () -> Unit, onClick: (Fli
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onClick(flight) } // TODO: add airport activity
             .padding(15.dp),
         content = {
-            Row(
-                modifier = Modifier.padding(10.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
             ) {
-                Column() {
-                    val icon = if (flight.flightType == "ARRIVAL") R.drawable.arrival
-                    else R.drawable.departure
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column() {
+                        val icon = if (flight.flightType == "ARRIVAL") R.drawable.arrival
+                        else R.drawable.departure
 
-                    Image(
-                        painter = painterResource(id = icon),
-                        contentDescription = "Flight", modifier = Modifier.size(70.dp)
-                    )
-                }
-                Column() {
-                    Text(text = stringResource(id = R.string.flight_type))
-                    Text(text = stringResource(id = R.string.departure))
-                    Text(text = stringResource(id = R.string.arrival))
-                    Text(text = stringResource(id = R.string.airport))
+                        Image(
+                            painter = painterResource(id = icon),
+                            contentDescription = "Flight", modifier = Modifier.size(70.dp)
+                        )
+                    }
+                    Column() {
+                        Text(text = stringResource(id = R.string.departure))
+                        Text(text = stringResource(id = R.string.arrival))
+                        Text(text = stringResource(id = R.string.airport))
+                    }
+
+                    Column() {
+                        Text(text = formatDateTime(LocalDateTime.parse(flight.departure)))
+                        Text(text = formatDateTime(LocalDateTime.parse(flight.arrival)))
+                        Text(text = flight.airportIcao)
+                    }
                 }
 
-                Column() {
-                    Text(text = flight.flightType)
-                    Text(text = formatDateTime(LocalDateTime.parse(flight.departure)))
-                    Text(text = formatDateTime(LocalDateTime.parse(flight.arrival)))
-                    Text(text = flight.airportIcao)
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically) {
+                    Button(onClick = {onClick(flight)}) {
+                        Text(text = "Mark as favourite")
+                    }
+                    Button(onClick = openAirportActivity) {
+                        Text(text = "Airport Details")
+                    }
                 }
             }
         }
@@ -111,10 +127,41 @@ fun FlightCard(flight: FlightDto, openAirportActivity: () -> Unit, onClick: (Fli
 fun FlightList(flights: List<FlightDto>, onClick: (FlightDto) -> Unit) {
     val mContext = LocalContext.current
 
+    val openDialog = remember { mutableStateOf(false)  }
+
+    if (openDialog.value) {
+
+        AlertDialog(
+            onDismissRequest = {
+                openDialog.value = false
+            },
+            title = {
+                Text(text = stringResource(id = R.string.success))
+            },
+            text = {
+                Text(text = stringResource(id = R.string.succesfullyMarked))
+            },
+            confirmButton = {
+            },
+            dismissButton = {
+                Button(
+
+                    onClick = {
+                        openDialog.value = false
+                    }) {
+                    Text(text = stringResource(id = R.string.dismiss))
+                }
+            }
+        )
+    }
+
     LazyColumn() {
         items(flights) { flight ->
             FlightCard(flight = flight,
-                onClick = onClick,
+                onClick = {
+                          onClick(it)
+                    openDialog.value = true
+                },
                 openAirportActivity = {
                     val intent = Intent(mContext, AirportActivity::class.java)
                     intent.putExtra("ICAO", flight.airportIcao)
