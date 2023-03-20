628b20918ca25938c241c1cc2cc5da40122e2459
S1mple133 <pavelescu_darius@yahoo.com>
2023-03-20 09:07:56 +0100

    added airport information

--- frontend/app/src/main/java/at/htl/airport_frontend/activity/AirportActivity.kt
+++ frontend/app/src/main/java/at/htl/airport_frontend/activity/AirportActivity.kt
@@ -3,16 +3,29 @@ package at.htl.airport_frontend.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import at.htl.airport_frontend.R
+import at.htl.airport_frontend.entity.AirportDto
 import at.htl.airport_frontend.ui.theme.AirportfrontendTheme
+import at.htl.airport_frontend.viewmodel.AirportActivityViewModel
+import at.htl.airport_frontend.viewmodel.FlightViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import java.time.LocalDateTime
 
+@AndroidEntryPoint
 class AirportActivity : ComponentActivity() {
+    private val mainViewModel by viewModels<AirportActivityViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,9 +36,9 @@ class AirportActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     if (intent.extras != null && intent.extras!!.getString("ICAO") != null) {
-                        AirportCard(icao = intent.extras!!.getString("ICAO")!!)
-                    }
-                    else {
+                        AirportPreview(mainViewModel.airportResponse)
+                        mainViewModel.getAirport(intent.extras!!.getString("ICAO")!!)
+                    } else {
                         UnknownICAO()
                     }
                 }
@@ -35,13 +48,59 @@ class AirportActivity : ComponentActivity() {
 }
 
 @Composable
-fun AirportCard(icao: String) {
-    Text(text = "Hello $icao!")
+fun AirportPreview(airport: AirportDto) {
+    Column(Modifier
+        .fillMaxWidth()) {
+        Row(Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically) {
+            Text(text = airport.name)
+        }
+        AirportCard(airport = airport)
+    }
+}
+
+@Composable
+fun AirportCard(airport: AirportDto) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column() {
+                        Text(text = stringResource(id = R.string.name))
+                        Text(text = stringResource(id = R.string.city))
+                        Text(text = stringResource(id = R.string.country))
+                        Text(text = stringResource(id = R.string.icao))
+                    }
+
+                    Column() {
+                        Text(text = airport.name)
+                        Text(text = airport.city)
+                        Text(text = airport.country)
+                        Text(text = airport.icao)
+                    }
+                }
+            }
+        }
+    )
 }
 
 @Composable
 fun UnknownICAO() {
-    Text(text = "ICAO not found!")
+    Text(text = "Airport not found!")
 }
 
 
@@ -55,8 +114,8 @@ fun PreviewUnknownICAO() {
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewAirportCard() {
+fun PreviewAirportPreview() {
     AirportfrontendTheme {
-        AirportCard("ASDF")
+        AirportPreview(AirportDto("icao", "name", "city", "country"))
     }
 }
--- frontend/app/src/main/java/at/htl/airport_frontend/boundary/FlightAPI.kt
+++ frontend/app/src/main/java/at/htl/airport_frontend/boundary/FlightAPI.kt
@@ -1,11 +1,16 @@
 package at.htl.airport_frontend.boundary
 
+import at.htl.airport_frontend.entity.AirportDto
 import at.htl.airport_frontend.entity.FlightDto
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface FlightAPI {
     @GET("flight")
     suspend fun getFlights(): List<FlightDto>
+
+    @GET("airport/{icao}")
+    suspend fun getAirport(@Path("icao") icao: String): AirportDto
 }
